392a3a65906dbe15a359ab410fe5069ab1511882
Jeronimo cardoso <jeronimo.alvescardoso@gmail.com>
2020-08-12 11:06:46 -0300

    primeiro commit

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+var krms_config ={			
+	'ApiUrl':"https://foods.thelondon.vip/singlemerchant/api",			
+	'DialogDefaultTitle':"Novilho de Ouro",			
+	'MerchantKeys':"c3b8c7410f86c2fd181157c5a0d2c0b8",
+	'debug': false
+};
+
